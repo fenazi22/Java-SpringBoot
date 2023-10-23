4c5c81f0ebd90a1625d2fec278050b68fa63248b
fenazi22 <fenazi22@gmail.com>
2023-10-23 22:24:42 +0300

    Week001-Day002

--- JavaBasics/src/Exercises001.java
+++ JavaBasics/src/Exercises001.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercises001 {
     public static void main(String[] args) {
 
-    Scanner input=new Scanner(System.in);
+       Scanner input=new Scanner(System.in);
 
 
 
