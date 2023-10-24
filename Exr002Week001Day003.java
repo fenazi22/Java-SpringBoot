d868bebfff54bfefd53fda67bb647b3ab97d1acc
Faisal Abdullah Alanazi <fenazi22@gmail.com>
2023-10-24 14:33:28 +0300

    Add files via upload

---
+++ Exr002Week001Day003.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class Exr002Week001Day003 {
+    public static void main(String[] args) {
+        Scanner inpuScanner=new Scanner(System.in);
+
+                        //  1- role
+//        System.out.print("Please Enter role: ");
+//        String role= inpuScanner.next();
+//        if (role.equalsIgnoreCase("Admin")){
+//            System.out.println("Welcome Admin ");
+//        } else if (role.equalsIgnoreCase("superuser")){
+//            System.out.println("Welcome  superuser");
+//
+//        }else {
+//            System.out.println("Welcome user");
+//        }
+//
+
+
+                            //  2- greatest
+//        System.out.print("Please Enter num1: ");
+//        int num1=inpuScanner.nextInt();
+//        System.out.print("Please Enter num2: ");
+//        int num2=inpuScanner.nextInt();
+//        System.out.print("Please Enter num3: ");
+//        int num3=inpuScanner.nextInt();
+//    if (num1>num2&&num1>num3){
+//        System.out.println("greatest: "+num1);
+//    } else if (num2>num1&&num2>num3){
+//        System.out.println("greatest: "+num2);
+//    }else {
+//        System.out.println("greatest: "+num3);
+//    }
+
+
+
+
+        // 3- Wednesday
+//        int max=1;
+//        int min=7;
+//        int randoms=(int)Math.floor(Math.random() * (max - min + 1) + min);
+//        switch (randoms){
+//            case 1:
+//                System.out.println("Sunday");
+//                break;
+//                    case 2:
+//                        System.out.println("Monday");
+//                break;
+//                    case 3:
+//                        System.out.println("Tuesday");
+//                break;
+//                    case 4:
+//                        System.out.println("Wednesday");
+//                break;
+//                    case 5:
+//                        System.out.println("Thursday");
+//                break;
+//                    case 6:
+//                        System.out.println("Friday");
+//                break;
+//                    case 7:
+//                        System.out.println("Saturday");
+//                break;
+//        }
+
+
+
+
+                //4  Grad
+
+//        System.out.print("Enter your numeric score: ");
+//    int score=inpuScanner.nextInt();
+//    if (score>=90 && score<=100) {
+//        System.out.println("Numeric score:"+score);
+//        System.out.println("A");
+//    } else if (score>=80 && score<=89) {
+//        System.out.println("Numeric score:"+score);
+//        System.out.println("B");
+//    } else if (score>=70 && score<=79) {
+//        System.out.println("Numeric score:"+score);
+//        System.out.println("C");
+//    } else if (score>=60 && score<=69) {
+//        System.out.println("Numeric score:"+score);
+//        System.out.println("D");
+//    } else if (score>=0 && score<=59) {
+//        System.out.println("Numeric score:"+score);
+//        System.out.println("F");
+//    }else{
+//        System.out.println("Numeric score:"+score);
+//        System.out.println("your Input Wrong !!");
+//    }
+
+                     //5- person's Age
+//        System.out.print("Enter your age: ");
+//    int age=inpuScanner.nextInt();
+//    if (age<=13 && age>0){
+//        System.out.println("your age is: "+age);
+//        System.out.println("Child");
+//    } else if (age>13 && age<=19) {
+//        System.out.println("your age is: "+age);
+//        System.out.println("inclusive");
+//    } else if (age>=20) {
+//        System.out.println("your age is: "+age);
+//        System.out.println("Adult");
+//
+//    }else if (age<=0){
+//        System.out.println("your age is: "+age);
+//        System.out.println("Wroing Input");
+//    }
+
+
+    }
+}
