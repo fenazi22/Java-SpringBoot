a149f27b33109c9f71c5338636a3ac4413749ea2
Faisal Abdullah Alanazi <fenazi22@gmail.com>
2023-10-26 01:00:40 +0300

    Add files via upload

---
+++ Week001Day004.java
@@ -0,0 +1,151 @@
+import java.util.Scanner;
+
+public class Week001Day004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+                     //1- number is a multiple of 3 and 5
+//        int num=100;
+//        for (int i = 0; i <num ; i++) {
+//            if (i%3==0 && i%5==0){
+//                System.out.println("Fizz");
+//            } else if (i%5==0) {
+//                System.out.println("Bizz");
+//            } else if (i%3==0) {
+//                System.out.println("FizzBuzz");
+//            }else {
+//                System.out.println(i);
+//            }
+//        }
+
+
+
+
+
+
+                    //2- revers text
+//        String text ="The quick brown fox";
+//        for (int i =  text.length()-1; i >=0 ; i--) {
+//            System.out.print(text.charAt(i));
+//
+//        }
+
+
+
+
+
+
+
+
+
+
+                 //3-   find the factorial value
+//        System.out.print("Enter number: ");
+//        int num =input.nextInt();
+//        int fact=1;
+//        for (int i = 1; i <=num ; i++) {
+//            fact*=i;
+//
+//        }
+//        System.out.printf("factorial value for number: %d is: %d",num,fact);
+
+
+                        //4- Power number
+//        System.out.println("\n\t\t----Power Without Method----\n");
+//        System.out.print("Please Enter First number(raised): ");
+//        double num3 = input.nextDouble();
+//        System.out.print("Please Enter Second number(Power):");
+//        int num4 = input.nextInt();
+//        double reslut=1.0;
+//        for (int i = 0; i < num4; i++) {
+//            reslut*=num3;
+//        }
+//        System.out.println(num3 + " raised to the power of " + num4 + " is: " + reslut);
+//        System.out.println("\n-\t\t-----------------\n");
+//        System.out.println("\n\t\t----Power With Method----\n");
+//        System.out.print("Please Enter First number(raised): ");
+//        double num1 = input.nextDouble();
+//        System.out.print("Please Enter Second number(Power):");
+//        int num2 = input.nextInt();
+//        double reslut1=1.0;
+//        reslut1=Math.pow(num1,num2);
+//        System.out.println(num1 + " raised to the power of " + num2 + " is: " + reslut1);
+
+
+
+
+
+
+
+//                //5-    sum of the even and odd
+//        System.out.print("Enter number: ");
+//        int num = input.nextInt();
+//        int sumEven = 0;
+//        int sumOdd = 0;
+//        if (num > 0) {
+//            for (int i = 0; i < num; i++) {
+//                if (i % 2 == 0) {
+//                    sumEven += i;
+//                } else if (i % 2 != 0) {
+//                    sumOdd += i;
+//                }
+//
+//            }
+//        } else {
+//            System.out.println("Your input wrong!!");
+//        }
+//
+//        System.out.println("\n total sum Even number is: " + sumEven + " , total sum odd number is: " + sumOdd);
+
+
+
+
+
+
+
+
+
+
+
+                          //6- Prime
+//        System.out.print("Please Enter number: ");
+//        int num = input.nextInt();
+//        if (num <= 1) {
+//            System.out.println("Not a prime number");
+//        } else {
+//            boolean isPrime = true;
+//            for (int i = 2; i < num; i++) {
+//                if (num % i == 0) {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//
+//            if (isPrime) {
+//                System.out.println(num + " is a prime number");
+//            } else {
+//                System.out.println(num + " is not a prime number");
+//
+//            }
+//        }
+
+
+
+
+
+        //7 -Weeks and Days
+//        for (int i = 1; i <=4 ; i++) {
+//            System.out.println("Week "+i);
+//            for (int j = 1; j <=7 ; j++) {
+//                System.out.println("Day"+j);
+//            }
+// }
+
+
+
+        //8-
+
+
+    }
+}
